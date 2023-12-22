868ac33417ada19e3ba5914d8e4433480ba30e64
YOGESH NAROLA <133344131+YogeshNarola01@users.noreply.github.com>
2023-12-22 10:39:14 +0530

    Add files via upload

---
+++ pr_6_q2.c
@@ -0,0 +1,44 @@
+/*Q.2 Write a Program to count the frequency of each character in a given string.
+Input:
+Enter any string: development
+Output:
+Frequency of each letter:
+d => 1
+e => 2
+v => 1
+l => 1
+o => 1
+p => 1
+m => 1
+n => 1
+/t => 1
+*/
+
+#include<stdio.h>
+ main(){
+	char str[100];
+	int i ,j ,count,num;
+	
+	printf("Enter string :");
+	scanf("%s",&str);
+	
+	for(i=0; str[i]; i++);
+	num=i;
+	
+	for(j=0; j<num; j++){
+		count=1;
+		if(str[j])
+		{
+			for(i=j+1; i<num; i++)
+			{
+				if(str[j]==str[i])
+				{
+					count++;
+					str[i]=NULL;
+				}
+			}
+		 printf(" '%c' = %d \n",str[j],count);
+		}
+		
+	}
+}
